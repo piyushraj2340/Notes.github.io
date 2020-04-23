0d81385c939506b5a4fd680f31af4d4e6c8e6772
piyushraj2340 <piyushraj2340@gmail.com>
2020-04-23 22:28:26 +0530

    update
    update

--- all.js
+++ all.js
@@ -1,28 +1,44 @@
 "use strict";
 
-function menuOpen(x) {
-  x.classList.toggle("open");
-  var menu1 = document.getElementsByClassName("menu1");
-  menu1[0].style.backgroundColor = "#55EE30";
-  var menu2 = document.getElementsByClassName("menu2");
-  menu2[0].style.backgroundColor = "#55EE30";
-  var menu3 = document.getElementsByClassName("menu3");
-  menu3[0].style.backgroundColor = "#55EE30";
-  var menu = document.getElementById("menu");
-  menu.style.backgroundColor = "red";
-  
+    // menu Open Function
+	 var count = (function () {
+	   var counter = 0;
+ 	   return function () {
+		   counter += 1;
+		   return counter;
+	   }
+   
+   })();
+function menuOpen() {
+   var elemMenu = document.getElementById("menu");
+   elemMenu.classList.toggle("open");
+	if(count()%2 == 0) {
+	  var elemAside = document.getElementsByTagName("aside");
+	  elemAside[0].style.display = "none";
+	  return count();
+   }
+    else {
+	  var elemAside = document.getElementsByTagName("aside");
+	  elemAside[0].style.display = "block";
+	  return count();
+	  }
 }
+
  
-  var menuEvent = document.getElementById("contener");
-  menuEvent.addEventListener("click", function() {
-	  menuClose(this);
-  });
-  
+ 
+ 
+  // close menu Event
+ 
+  var menuEvent = document.getElementById("menu");
+  menuEvent.addEventListener("click" , menuOpen);
+  menuEvent.addEventListener("click" , count);
+ /* 
   var menuEvent1 = document.getElementsByTagName("header");
   menuEvent1[0].addEventListener("click", function() {
 	  menuClose(this);
   });
   
+   // close menu function
 
 function menuClose(x) {
   var menu1 = document.getElementsByClassName("menu1");
@@ -33,10 +49,17 @@ function menuClose(x) {
   menu3[0].style.backgroundColor = "#000";
   var menu = document.getElementById("menu");
   menu.style.backgroundColor = "transparent";
+  x.classList.toggle("open");
 }
 
 
-
+  // height of aside 
+  
+  /* (function()  {
+      var elem = document.getElementsByTagName("aside");
+	  var 
+	  elem[0].style.height =  + "px";
+  })(); */
 
 
 
